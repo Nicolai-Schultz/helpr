ca7f10a9c96da7690188f7ac13611a1ab9a9019c
Nicolai Schultz <nicolaischultz@MacBook-Pro-tilhrende-Nicolai.local>
2024-06-14 15:00:12 +0200

    ny

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+STORAGE_DIR=/opt/render/project/.render
+
+echo "...Downloading Chrome"
+mkdir -p $STORAGE_DIR/chrome
+cd $STORAGE_DIR/chrome
+wget -P ./ https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -x ./google-chrome-stable_current_amd64.deb $STORAGE_DIR/chrome
+rm ./google-chrome-stable_current_amd64.deb
+cd $HOME/project/src # Make sure we return to where we weress
